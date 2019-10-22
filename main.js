3bb1f83d961ada0656f3df443382de287572a48a
Kristin <kbrandtsojo@gmail.com>
2019-10-22 10:17:39 -0600

    linked star wars page

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import { films } from '/assets/films.js'
+import { people } from '/assets/people.js'
+
+/*console.log(films[0].opening_crawl)
+
+let projects = document.querySelector('#welcome-section')
+
+films.forEach(function(film) {
+let filmDiv = document.createElement('div')
+let title = document.createElement('h1')
+let crawl = document.createElement('p')
+
+
+filmDiv.appendChild(title)
+filmDiv.appendChild(crawl)
+
+title.innerText = film.title
+crawl.innerText = film.opening_crawl
+
+projects.appendChild(filmDiv)
+})*/
