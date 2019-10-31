64ab0139bb5edce5f9672aa1f6dd7e69b3620786
Kristin <kbrandtsojo@gmail.com>
2019-10-31 11:10:23 -0600

    added pokemon images

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,7 +1,68 @@
-const allPokemon =(async () => {
+
+async function getAPIData(url){
+    try{
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
+.then(data => {
+for (const pokemon of data.results){
+    getAPIData(pokemon.url)
+    .then(pokedata => {
+        console.log(pokedata)
+    })
+}
+})
+console.log(theData)
+
+let mainArea = document.querySelector('main')
+
+function populateDOM(single_pokemon){
+    let pokeDiv = document.createElement('div')
+    let name = document.createElement('h3')
+    let pic = document.createElement('img')
+}
+
+let pokeNum = getPokeNumber(single_pokemon.id)
+
+name.textContent = single_pokemon.name
+
+pic.src = "../images/${pokeNum}.png"
+
+pokeDiv.appendChild(pic)
+pokeDiv.appendChild(name)
+
+mainArea.appendChild(pokeDiv)
+
+
+
+function getPokeNumber (id) {
+    if(id < 10) return '00${id}'
+    if id(id > 9 && id < 100) {
+        return '0${id}'
+    } else return id
+}
+
+/*const allPokemon =(async () => {
 const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
 const myJson = await response.json()
 console.log(JSON.stringify(myJson))
 })
 
-console.log(allPokemon())
+console.log(allPokemon())*/
+
+//for of vs for each vs for in
+//copy async code to code journal
+
+//fetch makes a get request
+
+//copy code from video
+
+//fix revert on the star wars characters page - look at video example
+
+//api calls and flip
