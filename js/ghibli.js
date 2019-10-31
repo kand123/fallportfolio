cd67f2010dbcf44ef5fc556895711c5f3be17cd2
Kristin <kbrandtsojo@gmail.com>
2019-10-30 23:01:51 -0600

    fetch setup for ghibli

--- js/ghibli.js
+++ js/ghibli.js
@@ -1,5 +1,14 @@
 /*const response = await fetch('http://example.com/movies.json');
 const myJson = await response.json();
-console.log(JSON.stringify(myJson));
+console.log(JSON.stringify(myJson));*/
 
 //something to start with?
+
+const url = 'https://ghibliapi.herokuapp.com/films/';
+function fetchData(){
+    fetch(url).then(function(rep){
+        return rep.json()
+    }).then(function(data){
+        console.log(data);
+    })
+}
