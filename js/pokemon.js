d2a1be35363a4dbf0656a3312ad3dd4181d510bb
Kristin <kbrandtsojo@gmail.com>
2019-11-04 23:21:29 -0700

    updated pokemon js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,6 +1,6 @@
 
 async function getAPIData(url){
-    try{
+    try {
         const response = await fetch(url)
         const data = await response.json()
         return data
@@ -11,8 +11,10 @@ async function getAPIData(url){
 
 const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
 .then(data => {
-for (const pokemon of data.results){
-    getAPIData(pokemon.url)
+    
+    for (const pokemon of data.results){
+        
+        getAPIData(pokemon.url)
     .then(pokedata => {
         console.log(pokedata)
     })
@@ -20,32 +22,35 @@ for (const pokemon of data.results){
 })
 console.log(theData)
 
+
 let mainArea = document.querySelector('main')
 
-function populateDOM(single_pokemon){
+function populateDOM(single_pokemon) {
     let pokeDiv = document.createElement('div')
     let name = document.createElement('h3')
     let pic = document.createElement('img')
-}
+
+//pokeDiv.setAttribute('class', 'charDivs')
+//pic.setAttribute('class', 'picDivs')
 
 let pokeNum = getPokeNumber(single_pokemon.id)
 
 name.textContent = single_pokemon.name
 
-pic.src = "../images/${pokeNum}.png"
+pic.src = `../images/${pokeNum}.png`
 
 pokeDiv.appendChild(pic)
 pokeDiv.appendChild(name)
 
 mainArea.appendChild(pokeDiv)
-
+}
 
 
 function getPokeNumber (id) {
-    if(id < 10) return '00${id}'
-    if id(id > 9 && id < 100) {
-        return '0${id}'
-    } else return id
+  if(id < 10) return `00${id}`
+  if(id > 9 && id < 100) {
+     return `0${id}`
+   } else return id
 }
 
 /*const allPokemon =(async () => {
@@ -61,8 +66,6 @@ console.log(allPokemon())*/
 
 //fetch makes a get request
 
-//copy code from video
 
-//fix revert on the star wars characters page - look at video example
 
 //api calls and flip
