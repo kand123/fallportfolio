f4b73eda996daff35d79004f99f91155e84c3a36
Kristin <kbrandtsojo@gmail.com>
2019-11-06 22:00:42 -0700

    cleaned up code on pokemon js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,56 +1,47 @@
-
-async function getAPIData(url){
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
-    } catch (error) {
-        console.error(error)
-    }
+async function getAPIData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
-.then(data => {
-    
-    for (const pokemon of data.results){
-        
-        getAPIData(pokemon.url)
-    .then(pokedata => {
-        console.log(pokedata)
+const theData = getAPIData("https://pokeapi.co/api/v2/pokemon/").then(data => {
+  for (const pokemon of data.results) {
+    getAPIData(pokemon.url).then(pokedata => {
+      populateDOM(pokedata);
     })
-}
+  }
 })
-console.log(theData)
+//console.log(theData)
 
-
-let mainArea = document.querySelector('main')
+let mainArea = document.querySelector("main")
 
 function populateDOM(single_pokemon) {
-    let pokeDiv = document.createElement('div')
-    let name = document.createElement('h3')
-    let pic = document.createElement('img')
+  let pokeDiv = document.createElement("div");
+  let name = document.createElement("h3");
+  let pic = document.createElement("img");
 
-pokeDiv.setAttribute('class', 'charDivs')
-pic.setAttribute('class', 'picDivs')
+  //pokeDiv.setAttribute("class", "charDivs");
+  //pic.setAttribute("class", "picDivs");
 
-let pokeNum = getPokeNumber(single_pokemon.id)
+  name.textContent = single_pokemon.name;
+  
+  let pokeNum = getPokeNumber(single_pokemon.id)
+  pic.src = `../assets/images/${pokeNum}.png`
 
-name.textContent = single_pokemon.name
-
-pic.src = `../assets/images/${pokeNum}.png`
-
-
-pokeDiv.appendChild(pic)
-pokeDiv.appendChild(name)
-mainArea.appendChild(pokeDiv)
+  pokeDiv.appendChild(pic);
+  pokeDiv.appendChild(name);
+  mainArea.appendChild(pokeDiv);
 }
 
-
-function getPokeNumber (id) {
-  if(id < 10) return `00${id}`
-  if(id > 9 && id < 100) {
-     return `0${id}`
-   } else return id
+function getPokeNumber(id) {
+  if (id < 10) return `00${id}`;
+  if (id > 9 && id < 100) {
+    return `0${id}`;
+  } else return id;
 }
 
 /*const allPokemon =(async () => {
@@ -65,7 +56,5 @@ console.log(allPokemon())*/
 //copy async code to code journal
 
 //fetch makes a get request
-
-
 
 //api calls and flip
