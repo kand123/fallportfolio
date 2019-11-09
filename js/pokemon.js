fa6ea5a757a2359f44c7ddfe7252f82ba26a084a
Kristin <kbrandtsojo@gmail.com>
2019-11-08 22:43:23 -0700

    fixed populateDOM on pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -12,36 +12,55 @@ const theData = getAPIData("https://pokeapi.co/api/v2/pokemon/").then(data => {
   for (const pokemon of data.results) {
     getAPIData(pokemon.url).then(pokedata => {
       populateDOM(pokedata);
-    })
+    });
   }
-})
+});
 //console.log(theData)
 
-let mainArea = document.querySelector("main")
+let mainArea = document.querySelector("main");
 
 function populateDOM(single_pokemon) {
+  let pokeScene = document.createElement("div");
+  let pokeCard = document.createElement("div");
+  let pokeFront = document.createElement("div");
+  let pokeBack = document.createElement("div");
   let pokeDiv = document.createElement("div");
-  let name = document.createElement("h3");
+  let name = document.createElement("p");
   let pic = document.createElement("img");
 
   //pokeDiv.setAttribute("class", "charDivs");
   //pic.setAttribute("class", "picDivs");
 
-  name.textContent = single_pokemon.name;
-  
-  let pokeNum = getPokeNumber(single_pokemon.id)
-  pic.src = `../assets/images/${pokeNum}.png`
+  pokeScene.setAttribute("class", "scene");
+  pokeCard.setAttribute("class", "card");
+  pokeFront.setAttribute("class", "charDivs card__face card__face--front");
+  pokeBack.setAttribute("class", "card__face card__face--back");
+  pic.setAttribute("class", "picDivs");
 
-  pokeDiv.appendChild(pic);
-  pokeDiv.appendChild(name);
-  mainArea.appendChild(pokeDiv);
+  let pokeNum = getPokeNumber(single_pokemon.id);
+  pokeFront.appendChild(name);
+  name.textContent = single_pokemon.name;
+
+  pic.src = `../assets/images/${pokeNum}.png`;
+  pokeFront.appendChild(pic)
+  pokeFront.appendChild(name)
+
+  pokeCard.appendChild(pokeFront)
+  pokeDiv.appendChild(pokeBack)
+  pokeScene.appendChild(pokeCard)
+
+  mainArea.appendChild(pokeScene)
+
+  pokeCard.addEventListener('click', function() {
+    pokeCard.classList.toggle('is-flipped');
+  });
 }
 
 function getPokeNumber(id) {
   if (id < 10) return `00${id}`;
   if (id > 9 && id < 100) {
-    return `0${id}`;
-  } else return id;
+    return `0${id}`
+  } else return id
 }
 
 /*const allPokemon =(async () => {
